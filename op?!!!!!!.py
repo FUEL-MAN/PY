88885f044d482c4dca2bd81cbea59fb7d2d26a4d
FUEL-MAN <79360135+FUEL-MAN@users.noreply.github.com>
2021-03-06 16:08:21 +0200

    Update op?!!!!!!.py

--- op?!!!!!!.py
+++ op?!!!!!!.py
@@ -3,10 +3,10 @@ from tkinter import *
   
   
 def clicked():
-    window = Tk()  #set ''window" like a main window
+    window = Tk()  #определяем это значение как главное окно 
     window.title("Д/З а может и не Д/З")  
-    window.geometry('600x600') #set window size
-    window.configure(bg='lightblue') #set color
+    window.geometry('600x600') #ставим размер окна 
+    window.configure(bg='lightblue') #ставим цвет фона
     lbl = Label(window, text="Я сделал д/з, но только 8 и 9,", font=("Arial Bold", 30), bg='lightblue')  
     lbl.grid(column=0, row=0)
     lbl = Label(window, text="я просто в матеше не шарю.", font=("Arial Bold", 30), bg='lightblue')  
@@ -21,7 +21,7 @@ def clickclack():
 window = Tk()  
 window.title("Д/З а может и не Д/З")  
 window.geometry('600x600')
-window.configure(bg='lightblue') #set color
+window.configure(bg='lightblue') #ставим цвет фона
 lbl = Label(window, text="Д/З", font=("Arial Bold", 40), bg='lightblue')  
 lbl.grid(column=0, row=0)
 btn = Button(window, text="Да это мой код", command=clicked, bg='lightblue')  
